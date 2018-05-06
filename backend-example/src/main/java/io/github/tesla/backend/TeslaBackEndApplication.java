335d855d4f7ce34326dcc5ab7f2b7a66c3e53f01
shiming.liu <shiming.liu@dianrong.com>
2018-05-06 11:41:34 +0800

    add demo for spring cloud

--- backend-example/src/main/java/io/github/tesla/backend/TeslaBackEndApplication.java
+++ backend-example/src/main/java/io/github/tesla/backend/TeslaBackEndApplication.java
@@ -2,8 +2,12 @@ package io.github.tesla.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
+@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class TeslaBackEndApplication {
   public static void main(String[] args) {
