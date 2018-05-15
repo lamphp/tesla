c191cd2120e4ba7911de6e168623a9e9fe7df69e
shiming.liu <shiming.liu@dianrong.com>
2018-05-15 20:35:54 +0800

    up

--- tesla-ops/src/main/resources/static/js/appjs/gateway/sharerule/add.js
+++ tesla-ops/src/main/resources/static/js/appjs/gateway/sharerule/add.js
@@ -2,61 +2,107 @@ var prefix = "/filter/sharerule";
 $(document).ready(function() {
   pageSetUp();
   var pagefunction = function() {
-    $("button[id^='submit']").each(function(index, element) {
-      $(this).click(function() {
-        var submitId = this.id;
-        var formId = 'ruleForm_' + submitId.split('_')[1];
-        $("#" + formId).validate({
-          rules: {
-            rule: {
-              required: true
-            },
-            url: {
-              required: true
-            }
-          },
-          messages: {
-            rule: {
-              required: "请输入规则"
-            },
-            url: {
-              required: "请输入匹配URL"
-            }
-          },
-          submitHandler: function(form) {
-            $(form).ajaxSubmit({
-              cache: true,
-              type: "post",
-              url: prefix + "/save",
-              data: $("#" + formId).serialize(),
-              async: false,
-              success: function() {
-                $("#" + formId).addClass('submited');
-                loadURL(prefix, $('#content'));
-              }
-            });
-          },
-          errorPlacement: function(error, element) {
-            error.insertAfter(element.parent());
+    var form = $("#ruleForm");
+    var ace = form.find("textarea").data('ace').editor.ace;
+    var value = ace.getValue();
+    $("#rule").val(value);
+    form.validate({
+      rules: {
+        rule: {
+          required: true
+        },
+        url: {
+          required: true
+        }
+      },
+      messages: {
+        rule: {
+          required: "请输入规则"
+        },
+        url: {
+          required: "请输入匹配URL"
+        }
+      },
+      submitHandler: function(form) {
+        $(form).ajaxSubmit({
+          cache: true,
+          type: "post",
+          url: prefix + "/save",
+          data: $(form).serialize(),
+          async: false,
+          success: function() {
+            $(form).addClass('submited');
+            loadURL(prefix, $('#content'));
           }
         });
-        $("#" + formId).submit();
-      })
+      },
+      errorPlacement: function(error, element) {
+        error.insertAfter(element.parent());
+      }
     });
-  }
-  var acefunction = function() {
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-      var activetab = $(e.target).attr("aria-controls");
-      $("#" + activetab).find("textarea").ace({
+
+  };
+  var swiperfunction = function() {
+    new Swiper('#certify .swiper-container', {
+      watchSlidesProgress: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      loop: true,
+      loopedSlides: 5,
+      autoplay: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      on: {
+        progress: function(progress) {
+          for (i = 0; i < this.slides.length; i++) {
+            var slide = this.slides.eq(i);
+            var slideProgress = this.slides[i].progress;
+            modify = 1;
+            if (Math.abs(slideProgress) > 1) {
+              modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
+            }
+            translate = slideProgress * modify * 260 + 'px';
+            scale = 1 - Math.abs(slideProgress) / 5;
+            zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
+            slide.transform('translateX(' + translate + ') scale(' + scale + ')');
+            slide.css('zIndex', zIndex);
+            slide.css('opacity', 1);
+            if (Math.abs(slideProgress) > 3) {
+              slide.css('opacity', 0);
+            }
+          }
+        },
+        setTransition: function(transition) {
+          for (var i = 0; i < this.slides.length; i++) {
+            var slide = this.slides.eq(i)
+            slide.transition(transition);
+          }
+
+        }
+      }
+    });
+  };
+  var choosefunction = function() {
+    try {
+      $(".choose").each(function() {
+        $(this).bind("click", function() {
+          var filter = $(this).data("compnent");
+          $("#filterType").val(filter);
+        });
+      });
+    } finally {
+      $("#ruleForm").find("textarea").ace({
         theme: 'idle_fingers',
         lang: 'text'
-      });
-    })
-    $("#URLParamHttpRequestFilter").find("textarea").ace({
-      theme: 'idle_fingers',
-      lang: 'text'
-    });
+      })
+    }
   }
   loadScript("js/plugin/jquery-form/jquery-form.min.js", pagefunction);
-  acefunction();
+  swiperfunction();
+  choosefunction();
 });
