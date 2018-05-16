e53d8857d3f6d8a79f444d926fbb44e36824db30
shiming.liu <shiming.liu@dianrong.com>
2018-05-16 10:22:59 +0800

    up

--- tesla-ops/src/main/resources/static/js/appjs/gateway/sharerule/edit.js
+++ tesla-ops/src/main/resources/static/js/appjs/gateway/sharerule/edit.js
@@ -2,50 +2,58 @@ var prefix = "/filter/sharerule";
 $(document).ready(function() {
   pageSetUp();
   var pagefunction = function() {
-    $("button[id^='submit']").each(function(index, element) {
-      $(this).click(function() {
-        var submitId = this.id;
-        var formId = 'ruleForm_' + submitId.split('_')[1];
-        $("#" + formId).validate({
-          rules: {
-            rule: {
-              required: true
-            },
-            url: {
-              required: true
-            }
-          },
-          messages: {
-            rule: {
-              required: "请输入规则"
-            },
-            url: {
-              required: "请输入匹配URL"
-            }
-          },
-          submitHandler: function(form) {
-            $(form).ajaxSubmit({
-              cache: true,
-              type: "post",
-              url: prefix + "/update",
-              data: $("#" + formId).serialize(),
-              async: false,
-              success: function() {
-                $("#" + formId).addClass('submited');
-                loadURL(prefix, $('#content'));
-              }
-            });
-          },
-          errorPlacement: function(error, element) {
-            error.insertAfter(element.parent());
+    $("#ruleForm").validate({
+      rules: {
+        rule: {
+          required: true
+        },
+        name: {
+          required: true
+        },
+        describe: {
+          required: true
+        },
+        filterType: {
+          required: true
+        }
+      },
+      messages: {
+        rule: {
+          required: "请输入详细规则内容"
+        },
+        name: {
+          required: "请输入规则名称"
+        },
+        describe: {
+          required: "请输入规则描述"
+        },
+        filterType: {
+          required: "请选择规则类型"
+        }
+      },
+      submitHandler: function(form) {
+        var ace = $("#ruleForm").find("textarea").data('ace').editor.ace;
+        var value = ace.getValue();
+        $("#rule").val(value);
+        $(form).ajaxSubmit({
+          cache: true,
+          type: "post",
+          url: prefix + "/update",
+          data: $("#ruleForm").serialize(),
+          async: false,
+          success: function() {
+            $("#ruleForm").addClass('submited');
+            loadURL(prefix, $('#content'));
           }
         });
-        $("#" + formId).submit();
-      })
+      },
+      errorPlacement: function(error, element) {
+        error.insertAfter(element.parent());
+      }
     });
   }
   var acefunction = function() {
-    $(".rule").ace({
+    $("#ruleForm").find("textarea").ace({
       theme: 'idle_fingers',
       lang: 'text'
     });
