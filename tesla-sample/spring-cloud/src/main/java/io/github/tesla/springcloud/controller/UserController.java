12499e6acc320eab0d482b361fe011a308b44210
shiming.liu <shiming.liu@dianrong.com>
2018-05-17 14:10:40 +0800

    add tesla-sample

---
+++ tesla-sample/spring-cloud/src/main/java/io/github/tesla/springcloud/controller/UserController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.tesla.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.tesla.springcloud.pojo.Users;
+
+/**
+ * @author liushiming
+ * @version BackEndController.java, v 0.0.1 2018年4月24日 上午11:51:00 liushiming
+ */
+@RestController
+@RequestMapping("springcloud")
+public class UserController {
+
+
+  @RequestMapping(value = "user", method = RequestMethod.POST)
+  public Users setterMessage1(@RequestBody Users users) {
+    return users;
+  }
+
+}
