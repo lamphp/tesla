f74a52594349c76a12ed6112fc3be24bda804eeb
shiming.liu <shiming.liu@dianrong.com>
2018-05-23 17:17:09 +0800

    up

--- tesla-gateway/src/main/java/io/github/tesla/gateway/cache/ApiAndFilterCacheComponent.java
+++ tesla-gateway/src/main/java/io/github/tesla/gateway/cache/ApiAndFilterCacheComponent.java
@@ -165,7 +165,7 @@ public class ApiAndFilterCacheComponent extends AbstractScheduleCache {
       String backEndPort = group.getBackendPort();
       final String backEndPath = group.getBackendPath();
       final String urlPath;
-      if (backEndPath != null) {
+      if (StringUtils.isNotBlank(backEndPath)) {
         urlPath = path(backEndPath) + path(apiClone.getPath());
       } else {
         urlPath = apiClone.getPath();
@@ -176,7 +176,7 @@ public class ApiAndFilterCacheComponent extends AbstractScheduleCache {
   }
 
   private String path(String path) {
-    if (path.startsWith("/") || path.startsWith("http://")) {
+    if (path.startsWith("/") || path.startsWith("http")) {
       return path;
     } else {
       return "/" + path;
